5c11af9217664f1d1fb3118299775884deb2d8bf
Jordan <jjohn468@uncc.edu>
2018-06-01 09:14:44 -0400

    Added a back button on the create new room page

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,6 +24,14 @@ public class HomeController {
     	session.setAttribute("roomManager", crm);
         return "home";
     }
-
+    @PostMapping("/")
+    public String postHome(Model model, HttpSession session) {
+    	model.addAttribute("sentBack",false);
+    	model.addAttribute(new Chatroom());
+    	ChatroomManager crm = new ChatroomManager();
+    	//crm.updateRooms();
+    	session.setAttribute("roomManager", crm);
+        return "home";
+    }
     
 }
