0a32e91073c9392a402153ca60827d2ef2a03ef7
Jordan <jjohn468@uncc.edu>
2018-06-03 13:13:55 -0400

    Added validaion to the chatroom message

--- src/main/resources/static/javascript/create-validation.js
+++ src/main/resources/static/javascript/create-validation.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to be ready
 $(function() {
   // Initialize form validation on the registration form.
-  // It has the name attribute "registration"
+  // It has the name attribute "createform"
   $("form[name='createform']").validate({
     // Specify validation rules
     rules: {
--- target/classes/static/javascript/chatroom-validation.js
+++ target/classes/static/javascript/chatroom-validation.js
@@ -1,23 +1,20 @@
-// Wait for the DOM to be ready
+
 $(function() {
   // Initialize form validation on the registration form.
-  // It has the name attribute "createform"
-  $("form[name='createform']").validate({
+  // It has the name attribute "messageform"
+  $("form[name='messageform']").validate({
     // Specify validation rules
     rules: {
       // The key name on the left side is the name attribute
       // of an input field. Validation rules are defined
       // on the right side
-      maxMembers: {
-    	  min: 2,
-    	  max: 20,
-    	  required: true,
-    	  digits: true
+      chatmessage: {
+    	  required:true
       }
     },
     // Specify validation error messages
     messages: {
-      roomId: "Please enter a room Id"
+      roomId: "Please enter a message"
     },
     // Make sure the form is submitted to the destination defined
     // in the "action" attribute of the form when valid
